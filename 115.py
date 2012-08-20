f9cd71e96abc8068c6538845c9a88db21b97fbea
ohalucky <ohalucky2@163.com>
2012-08-20 21:57:35 +0800

    Update 115.py
    输出到桌面文件

--- 115.py
+++ 115.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 accounts_here = [
-    {"account":'115115115','password':'000000'},
-    {"account":'116116116','password':'000000'},
-]
+    {'account':'13666666666','password':'xxxxxxxx'},
+    ]
+filename = 'C:\\Users\\(username)\\Desktop\\here115.txt'#输出文件名,username改为用户名，去掉括号
+
 
 import urllib
 import urllib2
 import cookielib
 import json
 import re,time
+import os,sys,datetime
+
+startTime = time.localtime()
+strStartTime = time.strftime('%Y-%m-%d %H:%M:%S', startTime)
+f=open('here115.txt','a')
+f.write(strStartTime+':\t')
+#f.close()
 
 class Here115:
   def __init__(self):
@@ -28,14 +36,21 @@ class Here115:
       fd = self.opener.open(req)
     except Exception, e:
       print('网络连接错误！')
+      f.write('网络连接错误！\n')
+      f.close()
       return False
     res=fd.read()
     fd.close()
     if re.search('location\.href="http://115.com"', res) == None:
-      print('%s 密码不正确！\n' % username)
+      str='%s 密码不正确！\n' % username
+      print str
+      f.write(str+'\n')
+      f.close()
       return False
     else:
-      print('%s 登陆成功，准备摇奖..   ' % username),
+      str='%s 登陆成功，准备摇奖..   ' % username
+      print str,
+      f.write(str+'\n')
       return True
 
   def pick_space(self):
@@ -47,6 +62,8 @@ class Here115:
     token = re.search("take_token:\s'(\w+)'", token_page)
     if not token:
       print('今天已经摇过了...')
+      f.write('今天已经摇过了...\n')
+      f.close()
       return
 
     url = 'http://115.com/?ct=ajax_user&ac=pick_space&token=' + token.group(1)
@@ -56,9 +73,13 @@ class Here115:
     fd.close()
     if res_json['state'] == False:
       print('摇奖失败！')
+      f.write('摇奖失败！\n')
+      f.close()
       return
     str=u'\n获取空间：%s, 总空间：%s, 已使用：%s, 获取雨露：%d\n' % (res_json['picked'], res_json['total_size'], res_json['used_percent'], res_json['exp'])
     print str.encode("u8")
+    f.write(str+'\n')
+    f.close()
 
 
 if __name__ == '__main__':
